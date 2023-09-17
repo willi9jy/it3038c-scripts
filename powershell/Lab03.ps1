813605e1d77c93e120e12cc5fb1aba67db977028
willi9jy <75701893+willi9jy@users.noreply.github.com>
2023-09-17 15:28:55 -0400

    Add files via upload

---
+++ powershell/Lab03.ps1
@@ -0,0 +1,9 @@
+﻿$currentUserName = $env:username
+$hostname = $env:COMPUTERNAME
+$psVersion =$PSVersionTable.PSVersion
+$CurremtDateTime = Get-Date
+$IP = getIP atusg
+$body =“This machine’s IP is $IP. User is $currentUserName. The hostname is $hostname. Powershell version is $psVersion. Today's date and time is $CurremtDateTime. "
+Write-Host $body
+
+$body | Out-file c:\lab03.txt
---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,13 @@
+﻿$Hello = "Hello, PowerShell"
+Write-host($Hello)
+function getIP{ 
+
+    (get-netipaddress).ipv4address | Select-String "192*" 
+
+} 
+write-host(getIP) gir st
+$IP = getIP atusg
+
+Write-Host(“This machine’s IP is $IP”) 
+
+Write-Host(“This machine’s IP is {0}” -f $IP) v
